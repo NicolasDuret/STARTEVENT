3a2f7575c5e330a1872eefa2a01540ef83846f92
Nathalie D-Bonheur <n.durassierbonheur@epsi.fr>
2019-01-11 15:19:59 +0100

    Rectifiactions

--- tableau_event.php
+++ tableau_event.php
@@ -1,13 +1,22 @@
 <?php include_once "header.php" ?>
 
+<?php require_once 'Config.php'?>
+
 <?php
-require_once 'Config.php'
+$db = new PDO("mysql:host=" . Config::SERVEUR . ";dbname=" . Config::BASE, Config::USER, Config::MDP);
 
-$db = new PDO("mysql:host=" . Config::SERVEUR . "dbname=" . Config::BASE, Config::USER, Config::MDP);
-$r = $db -> prepare ("select prenom, versement, difference from personnes");
+$r = $db -> prepare ("select prenom, versement, description from personnes");
 $r->execute();
+$lignes = $r->fetchAll();
 
-$personne = $r->fetchAll();
+$r2 = $db -> prepare ("select SUM(versement) as somme from personnes");
+$r2->execute();
+$resultat = $r2->fetch();
+
+$r3 = $db-> prepare ("select AVG(versement) as moyenne from personnes");
+$r3->execute();
+$moyenne = $r3->fetch();
+
 ?>
 
 <h1>Startevent</h1>
@@ -26,19 +35,41 @@ $personne = $r->fetchAll();
     foreach($lignes as $ligne){
         ?>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>TOTAL</td>
-            <td></td>
-            <td></td>
+            <td><?php echo $ligne["prenom"]?></td>
+            <td><?php echo $ligne["versement"]?></td>
+            <td><?php echo $ligne["description"]?></td>
         </tr>
     <?php } ?>
+    <tr>
+        <td>TOTAL</td>
+        <td><?php echo $resultat["somme"] ?></td>
+        <td><?php echo round($moyenne["moyenne"]) ?></td>
+
+    </tr>
     </tbody>
 </table>
+
+<table>
+    <thead>
+    <tr>
+        <th>Diffirences</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <td>
+        <tr>
+            <?php
+            foreach($lignes as $ligne{
+                echo $difference=$ligne["versement"]-$moyenne["moyenne"];
+            }
+            ?>
+        </tr>
+    </td>
+    </tbody>
+</table>
+
+
 <a href="ajouter_personne.php" class="btn-large">
     <i class="material-icons left">add_circle_outline</i>
     Ajouter une personne
